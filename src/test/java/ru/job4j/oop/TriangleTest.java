09085dbe96c06afe67d25b829f249d40e7d7d514
UmedD <umedjon721@gmail.com>
2023-10-06 11:15:17 +0500

    4. Расстояние между точками в трехмерном пространстве

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -27,4 +27,15 @@ public class TriangleTest {
         double expected = 4;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
+
+    @Test
+    public void when012and200and241Then() {
+        Point a = new Point(0, 1, 2);
+        Point b = new Point(2, 0, 0);
+        Point c = new Point(2, 4, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 4;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
 }
