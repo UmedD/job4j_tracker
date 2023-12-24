520f480e9726e83efc6e5acd158a62b68cebfdbb
UmedD <umedjon721@gmail.com>
2023-12-24 15:22:46 +0500

    10. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,11 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        Item item2 = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String time = item.getCreated().format(formatter);
+        String time2 = item2.toString();
         System.out.println(time);
+        System.out.println(time2);
     }
 }
