bb8092039682ab78b62c89ed230fdc4974be8657
UmedD <umedjon721@gmail.com>
2024-01-10 23:55:42 +0500

    0.4. Пользовательские исключения

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result < 0) {
+            throw new ElementNotFoundException("элемент не найден");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"aa", "bbb"}, "aa"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
